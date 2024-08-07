9893ba5094ee84a2ac6f0973ee6f38e14f2a81b3
Jack <34489450+fenjalien@users.noreply.github.com>
2024-08-07 11:49:00 +0100

    Add API section (#8)

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -4,38 +4,39 @@
 // There are various equivalent ways to declare your Docusaurus config.
 // See: https://docusaurus.io/docs/api/docusaurus-config
 
-import remarkMath from 'remark-math';
-import rehypeKatex from 'rehype-katex';
+import remarkMath from "remark-math";
+import rehypeKatex from "rehype-katex";
 
-import typstRender from './src/remark/typst.js'
-import * as TypstThemes from './src/theme/typst.ts'
+import typstRender from "./src/remark/typst-render.js";
+import TypstTheme from "./src/theme/typst.ts";
 
 const config = {
-  title: 'CeTZ Documentation',
+  title: "CeTZ Documentation",
 
-  url: 'https://cetz-package.github.io/',
-  baseUrl: '/docs/',
+  url: "https://cetz-package.github.io/",
+  baseUrl: "/docs/",
 
-  organizationName: 'cetz-package',
-  projectName: 'docs',
+  organizationName: "cetz-package",
+  projectName: "docs",
 
-  onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "warn",
 
   i18n: {
-    defaultLocale: 'en',
-    locales: ['en'],
+    defaultLocale: "en",
+    locales: ["en"],
   },
 
   plugins: ["@orama/plugin-docusaurus-v3"],
 
   presets: [
     [
-      'classic',
+      "classic",
       {
         docs: {
+          path: "cetz/docs",
           routeBasePath: "/",
-          sidebarPath: './sidebars.js',
+          sidebarPath: "./sidebars.js",
           editUrl: "https://github.com/cetz-package/docs/blob/main",
 
           beforeDefaultRemarkPlugins: [typstRender],
@@ -44,59 +45,84 @@ const config = {
         },
         // blog: false,
         theme: {
-          customCss: ['./src/css/custom.css', './src/css/parameter.css', './src/css/type.css', './src/css/code.css'],
+          customCss: [
+            "./src/css/custom.css",
+            "./src/css/parameter.css",
+            "./src/css/type.css",
+            "./src/css/code.css",
+            "./src/css/function.css",
+          ],
         },
       },
     ],
   ],
 
-  themeConfig:
-  {
+  themeConfig: {
     colorMode: {
       respectPrefersColorScheme: true,
     },
     docs: {
       sidebar: {
-        hideable: true
-      }
+        hideable: true,
+      },
     },
     navbar: {
-      title: 'CeTZ Documentation',
+      title: "CeTZ Documentation",
       items: [
         {
-          type: 'docSidebar',
-          sidebarId: 'docs',
-          label: 'Docs',
-        }
+          type: "docSidebar",
+          sidebarId: "docs",
+          label: "Docs",
+        },
+        {
+          type: "docSidebar",
+          sidebarId: "api",
+          label: "API",
+        },
       ],
     },
     footer: {
       links: [
         {
           label: "Homepage",
-          href: "https://cetz-package.github.io"
+          href: "https://cetz-package.github.io",
         },
         {
           label: "Github",
-          href: "https://github.com/cetz-package/cetz"
-        }
-      ]
+          href: "https://github.com/cetz-package/cetz",
+        },
+      ],
     },
 
     prism: {
-      theme: TypstThemes.light,
-      darkTheme: TypstThemes.dark,
+      theme: TypstTheme,
     },
   },
   stylesheets: [
     {
-      href: 'https://cdn.jsdelivr.net/npm/katex@0.13.24/dist/katex.min.css',
-      type: 'text/css',
+      href: "https://cdn.jsdelivr.net/npm/katex@0.13.24/dist/katex.min.css",
+      type: "text/css",
       integrity:
-        'sha384-odtC+0UGzzFL/6PNoE8rX/SPcQDXBJ+uRepguP4QkPCm2LBxH3FA3y+fKSiJ+AmM',
-      crossorigin: 'anonymous',
+        "sha384-odtC+0UGzzFL/6PNoE8rX/SPcQDXBJ+uRepguP4QkPCm2LBxH3FA3y+fKSiJ+AmM",
+      crossorigin: "anonymous",
     },
   ],
+  markdown: {
+    preprocessor: ({ filePath, fileContent }) => {
+      return fileContent.replaceAll(
+        /\{\{(\w+)\}\}/g,
+        (_, type) => `<Type>${type}</Type>`,
+      );
+      // .replaceAll(/@@generated\/([\w-\/]+)/g, (_, path) => {
+      //   const split = path.split("/");
+      //   // console.log(split, split[split.length - 1]);
+      //   const output = `## ${split[split.length - 1]}\nimport Imported from "@site/cetz/docs/_generated/${path}.mdx";\n<Imported />
+      //   `;
+      //   // console.log(output);
+      //   return output;
+      // });
+    },
+  },
 };
 
 export default config;
